4f873a144b35daca99d8798b31327f68ea167104
miyako <keisuke.miyako@4d.com>
2012-07-04 09:36:21 +0900

    Added multi part post over http 1.0

--- OAuth/functions_curl.cpp
+++ OAuth/functions_curl.cpp
@@ -131,12 +131,27 @@ void _cURL(sLONG_PTR *pResult, PackagePtr pParams)
 	C_BLOB Param4InData;
 	C_BLOB Param5OutData;
 	C_LONGINT returnValue;
-	
+	ARRAY_TEXT Param6DataNames;
+	ARRAY_TEXT Param7DataTypes;
+	ARRAY_TEXT Param8DataFileNames;    
+    PA_Variable Param9DataObjects;
+    
 	Param1Url.fromParamAtIndex(pParams, 1);
 	Param2Keys.fromParamAtIndex(pParams, 2);
 	Param3Values.fromParamAtIndex(pParams, 3);
 	Param4InData.fromParamAtIndex(pParams, 4);
+    
+	Param6DataNames.fromParamAtIndex(pParams, 6);
+	Param7DataTypes.fromParamAtIndex(pParams, 7);
+	Param8DataFileNames.fromParamAtIndex(pParams, 8);    
+	Param9DataObjects = *((PA_Variable*) pParams[8]);
 	
+    unsigned int size6, size7, size8, size9;
+    size6 = Param6DataNames.getSize();
+    size7 = Param7DataTypes.getSize();
+    size8 = Param8DataFileNames.getSize();
+    size9 = (Param9DataObjects.uValue.fArray.fNbElements) + 1;   
+    
 	CURL *curl = curl_easy_init();
 	CURLcode error = CURLE_OK;
 	
@@ -144,18 +159,22 @@ void _cURL(sLONG_PTR *pResult, PackagePtr pParams)
 		
 		CUTF8String url;
 		Param1Url.copyUTF8String(&url);	
-		
+        
 		error = curl_easy_setopt(curl, CURLOPT_READFUNCTION, _cURL_read_data);
 		error = curl_easy_setopt(curl, CURLOPT_READDATA, &Param4InData);
 		
 		error = curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, _cURL_write_data);
 		error = curl_easy_setopt(curl, CURLOPT_WRITEDATA, &Param5OutData);
-		
+        
 		struct curl_slist *values_CURLOPT_MAIL_RCPT = NULL;
-		struct curl_slist *values_CURLOPT_AUTHORIZATION = NULL;
+        
+		struct curl_slist *values_CURLOPT_HTTPHEADER = NULL;
+        
+		struct curl_httppost *post_CURLOPT_HTTPPOST = NULL;
+		struct curl_httppost *last_CURLOPT_HTTPPOST = NULL;
 		
 		if(Param2Keys.getSize() == Param3Values.getSize()){
-			
+            
 			CURLoption option;
 			CUTF8String value;			
 			
@@ -165,167 +184,231 @@ void _cURL(sLONG_PTR *pResult, PackagePtr pParams)
 				Param3Values.copyUTF8StringAtIndex(&value, i);
 				
 				switch (option) {
-						//no action;require linked list						
-					case CURLOPT_HTTPPOST:							
-					case CURLOPT_HTTPHEADER:							
-						break;	
-						
+					case 0://typically element #0 of array
+                        break;
+					case 24://CURLOPT_HTTPPOST							
+                        if((Param9DataObjects.fType == eVK_ArrayPicture)
+                           && (size6 == size7)
+                           && (size7 == size8)                           
+                           && (size8 == size9)){
+                            for(unsigned int i = 0; i < size8; ++i){
+                                
+                                CUTF8String formName;
+                                Param6DataNames.copyUTF8StringAtIndex(&formName, i);
+                                
+                                CUTF8String formType;
+                                Param7DataTypes.copyUTF8StringAtIndex(&formType, i); 
+                                
+                                CUTF8String formFileName;
+                                Param8DataFileNames.copyUTF8StringAtIndex(&formFileName, i);  
+                                
+                                C_TEXT codec;
+                                codec.setUTF8String(&formType);
+                                PA_Unistring u = PA_CreateUnistring((PA_Unichar *)codec.getUTF16StringPtr());
+                                
+                                PA_Variable params[3];
+                                params[0] = PA_CreateVariable(eVK_Picture);            
+                                params[1] = PA_CreateVariable(eVK_Blob);
+                                params[2] = PA_CreateVariable(eVK_Unistring);
+                                
+                                PA_Picture picture = PA_GetPictureInArray(Param9DataObjects, i);
+                                PA_SetPictureVariable(&params[0], picture);
+                                PA_SetBlobVariable(&params[1], 0, 0);                                    
+                                PA_SetStringVariable(&params[2], &u);
+                                
+                                PA_ExecuteCommandByID(692, params, 3);
+                                
+                                PA_Handle h = PA_GetBlobHandleVariable(params[1]);
+                                
+                                if(PA_GetHandleSize(h)){
+                                    
+                                    curl_formadd(&post_CURLOPT_HTTPPOST, 
+                                                 &last_CURLOPT_HTTPPOST, 
+                                                 CURLFORM_COPYNAME, formName.c_str(), 
+                                                 CURLFORM_NAMELENGTH, formName.size(),
+                                                 CURLFORM_COPYCONTENTS, PA_LockHandle(h),
+                                                 CURLFORM_CONTENTSLENGTH, PA_GetHandleSize(h),
+                                                 CURLFORM_CONTENTTYPE, formType.c_str(), 
+                                                 CURLFORM_END);
+                                    
+                                    PA_UnlockHandle(h);
+                                    
+                                }
+                                
+                                PA_DisposeUnistring(&u); 
+                            }        
+                            
+                        }  
+                        
+                        if(post_CURLOPT_HTTPPOST){
+                            curl_easy_setopt(curl, CURLOPT_HTTPPOST, post_CURLOPT_HTTPPOST);
+                            values_CURLOPT_HTTPHEADER = curl_slist_append(values_CURLOPT_HTTPHEADER, 
+                                                                          (const char *)"Expect:");                             
+                        } 
+                        
+						break;
+                        
 					case 2://CURLOPT_AUTHORIZATION
 						
-						values_CURLOPT_AUTHORIZATION = curl_slist_append(values_CURLOPT_AUTHORIZATION, (const char *)(CUTF8String((const uint8_t *)"Authorization: ") + value).c_str());  
-						curl_easy_setopt(curl, CURLOPT_HTTPHEADER, values_CURLOPT_AUTHORIZATION);
-						
-						break;		
+						values_CURLOPT_HTTPHEADER = curl_slist_append(values_CURLOPT_HTTPHEADER, 
+                                                                      (const char *)(CUTF8String((const uint8_t *)"Authorization: ") + value).c_str());  
+                        
+						break;	
+                        
 						//no action;dedicated methods exist 						
-					case CURLOPT_URL:
-					case CURLOPT_WRITEFUNCTION:	
-					case CURLOPT_WRITEDATA:	
-					case CURLOPT_READFUNCTION:							
-					case CURLOPT_READDATA:							
-						break;
+                        //					case CURLOPT_URL:
+                        //					case CURLOPT_WRITEFUNCTION:	
+                        //					case CURLOPT_WRITEDATA:	
+                        //					case CURLOPT_READFUNCTION:							
+                        //					case CURLOPT_READDATA:							
+                        //						break;
+                        
 						//integer options
-					case CURLOPT_HEADER:							
-					case CURLOPT_NOSIGNAL:						
-					case CURLOPT_TIMEVALUE:	
-					case CURLOPT_TIMECONDITION:	
-					case CURLOPT_MAXFILESIZE_LARGE:	
-					case CURLOPT_MAXFILESIZE:	
-					case CURLOPT_INFILESIZE_LARGE:	
-					case CURLOPT_INFILESIZE:	
-					case CURLOPT_NOBODY:	
-					case CURLOPT_FILETIME:	
-					case CURLOPT_RESUME_FROM_LARGE:	
-					case CURLOPT_RESUME_FROM:	
-					case CURLOPT_CRLF:	
-					case CURLOPT_PROXY_TRANSFER_MODE:	
-					case CURLOPT_TRANSFERTEXT:		
-					case CURLOPT_NEW_DIRECTORY_PERMS:					
-					case CURLOPT_NEW_FILE_PERMS:						
-					case CURLOPT_SSH_AUTH_TYPES:							
-					case CURLOPT_GSSAPI_DELEGATION:							
-					case CURLOPT_SSL_OPTIONS:								
-					case CURLOPT_SSL_SESSIONID_CACHE:								
-					case CURLOPT_CERTINFO:							
-					case CURLOPT_SSL_VERIFYHOST:	
-					case CURLOPT_SSL_VERIFYPEER:							
-					case CURLOPT_SSLVERSION:	
-					case CURLOPT_SSLENGINE_DEFAULT:							
-					case CURLOPT_ACCEPTTIMEOUT_MS:							
-					case CURLOPT_USE_SSL:						
-					case CURLOPT_CONNECT_ONLY:						
-					case CURLOPT_IPRESOLVE:	
-					case CURLOPT_CONNECTTIMEOUT_MS:							
-					case CURLOPT_CONNECTTIMEOUT:	
-					case CURLOPT_FORBID_REUSE:	
-					case CURLOPT_FRESH_CONNECT:	
-					case CURLOPT_MAXCONNECTS:	
-					case CURLOPT_MAX_RECV_SPEED_LARGE:	
-					case 146:							
-					case CURLOPT_MAX_SEND_SPEED_LARGE:
-					case 145:						
-					case CURLOPT_LOW_SPEED_TIME:	
-					case CURLOPT_LOW_SPEED_LIMIT:	
-					case CURLOPT_TIMEOUT_MS:							
-					case CURLOPT_TIMEOUT:														
-					case CURLOPT_RTSP_REQUEST:						
-					case CURLOPT_RTSP_CLIENT_CSEQ:								
-					case CURLOPT_RTSP_SERVER_CSEQ:						
-					case CURLOPT_TFTP_BLKSIZE:					
-					case CURLOPT_DIRLISTONLY:							
-					case CURLOPT_APPEND:	
-					case CURLOPT_FTP_USE_EPRT:	
-					case CURLOPT_FTP_USE_EPSV:	
-					case CURLOPT_FTP_USE_PRET:							
-					case CURLOPT_FTP_RESPONSE_TIMEOUT:	
-					case CURLOPT_FTP_SKIP_PASV_IP:							
-					case CURLOPT_FTPSSLAUTH:	
-					case CURLOPT_FTP_SSL_CCC:						
-					case CURLOPT_FTP_ACCOUNT:							
-					case CURLOPT_FTP_FILEMETHOD:						
-					case CURLOPT_FTP_CREATE_MISSING_DIRS:	
-					case CURLOPT_AUTOREFERER:
-					case CURLOPT_FOLLOWLOCATION:
-					case CURLOPT_UNRESTRICTED_AUTH:
-					case CURLOPT_MAXREDIRS:
-					case CURLOPT_POSTREDIR:							
-					case CURLOPT_POSTFIELDSIZE:
-					case CURLOPT_POSTFIELDSIZE_LARGE:
-					case CURLOPT_COOKIESESSION:
-					case CURLOPT_HTTPGET:
-					case CURLOPT_HTTP_VERSION:
-					case CURLOPT_HTTP_CONTENT_DECODING:					
-					case CURLOPT_HTTP_TRANSFER_DECODING:							
-					case CURLOPT_IGNORE_CONTENT_LENGTH:
-					case CURLOPT_PROXYAUTH:
-					case CURLOPT_TLSAUTH_TYPE:							
-					case CURLOPT_HTTPAUTH:
-					case CURLOPT_NETRC:
-					case CURLOPT_TCP_KEEPINTVL:						
-					case CURLOPT_TCP_KEEPIDLE:						
-					case CURLOPT_TCP_KEEPALIVE:
-					case CURLOPT_ADDRESS_SCOPE:						
-					case CURLOPT_TCP_NODELAY:
-					case CURLOPT_PORT:
-					case CURLOPT_BUFFERSIZE:
-					case CURLOPT_DNS_CACHE_TIMEOUT:
-					case CURLOPT_LOCALPORTRANGE:							
-					case CURLOPT_LOCALPORT:						
-					case CURLOPT_SOCKS5_GSSAPI_NEC:							
-					case CURLOPT_HTTPPROXYTUNNEL:						
-					case CURLOPT_PROXYTYPE:						
-					case CURLOPT_PROXYPORT:						
-					case CURLOPT_REDIR_PROTOCOLS:						
-					case CURLOPT_PROTOCOLS:
+					case 42://CURLOPT_HEADER:							
+					case 99://CURLOPT_NOSIGNAL:						
+					case 34://CURLOPT_TIMEVALUE:	
+					case 33://CURLOPT_TIMECONDITION:	
+					case 117://CURLOPT_MAXFILESIZE_LARGE:	
+					case 114://CURLOPT_MAXFILESIZE:	
+					case 115://CURLOPT_INFILESIZE_LARGE:	
+					case 14://CURLOPT_INFILESIZE:	
+					case 44://CURLOPT_NOBODY:	
+					case 69://CURLOPT_FILETIME:	
+					case 116://CURLOPT_RESUME_FROM_LARGE:	
+					case 21://CURLOPT_RESUME_FROM:	
+					case 27://CURLOPT_CRLF:	
+					case 166://CURLOPT_PROXY_TRANSFER_MODE:	
+					case 53://CURLOPT_TRANSFERTEXT:		
+					case 160://CURLOPT_NEW_DIRECTORY_PERMS:					
+					case 159://CURLOPT_NEW_FILE_PERMS:						
+					case 151://CURLOPT_SSH_AUTH_TYPES:							
+					case 210://CURLOPT_GSSAPI_DELEGATION:							
+					case 216://CURLOPT_SSL_OPTIONS:								
+					case 150://CURLOPT_SSL_SESSIONID_CACHE:								
+					case 172://CURLOPT_CERTINFO:							
+					case 81://CURLOPT_SSL_VERIFYHOST:	
+					case 64://CURLOPT_SSL_VERIFYPEER:							
+					case 32://CURLOPT_SSLVERSION:	
+					case 90://CURLOPT_SSLENGINE_DEFAULT:							
+					case 212://CURLOPT_ACCEPTTIMEOUT_MS:							
+					case 119://CURLOPT_USE_SSL:						
+					case 141://CURLOPT_CONNECT_ONLY:						
+					case 113://CURLOPT_IPRESOLVE:	
+					case 156://CURLOPT_CONNECTTIMEOUT_MS:							
+					case 78://CURLOPT_CONNECTTIMEOUT:	
+					case 75://CURLOPT_FORBID_REUSE:	
+					case 74://CURLOPT_FRESH_CONNECT:	
+					case 71://CURLOPT_MAXCONNECTS:	
+					case 146://CURLOPT_MAX_RECV_SPEED_LARGE:							
+					case 145://CURLOPT_MAX_SEND_SPEED_LARGE:						
+					case 20://CURLOPT_LOW_SPEED_TIME:	
+					case 19://CURLOPT_LOW_SPEED_LIMIT:	
+					case 155://CURLOPT_TIMEOUT_MS:							
+					case 13://CURLOPT_TIMEOUT:														
+					case 189://CURLOPT_RTSP_REQUEST:						
+					case 193://CURLOPT_RTSP_CLIENT_CSEQ:								
+					case 194://CURLOPT_RTSP_SERVER_CSEQ:						
+					case 178://CURLOPT_TFTP_BLKSIZE:					
+					case 48://CURLOPT_DIRLISTONLY:							
+					case 50://CURLOPT_APPEND:	
+					case 106://CURLOPT_FTP_USE_EPRT:	
+					case 85://CURLOPT_FTP_USE_EPSV:	
+					case 188://CURLOPT_FTP_USE_PRET:							
+					case 112://CURLOPT_FTP_RESPONSE_TIMEOUT:	
+					case 137://CURLOPT_FTP_SKIP_PASV_IP:							
+					case 129://CURLOPT_FTPSSLAUTH:	
+					case 154://CURLOPT_FTP_SSL_CCC:						
+					case 134://CURLOPT_FTP_ACCOUNT:							
+					case 138://CURLOPT_FTP_FILEMETHOD:						
+					case 110://CURLOPT_FTP_CREATE_MISSING_DIRS:	
+					case 58://CURLOPT_AUTOREFERER:
+					case 52://CURLOPT_FOLLOWLOCATION:
+					case 105://CURLOPT_UNRESTRICTED_AUTH:
+					case 68://CURLOPT_MAXREDIRS:
+					case 161://CURLOPT_POSTREDIR:							
+					case 60://CURLOPT_POSTFIELDSIZE:
+					case 120://CURLOPT_POSTFIELDSIZE_LARGE:
+					case 96://CURLOPT_COOKIESESSION:
+					case 80://CURLOPT_HTTPGET:
+					case 84://CURLOPT_HTTP_VERSION:
+					case 158://CURLOPT_HTTP_CONTENT_DECODING:					
+					case 157://CURLOPT_HTTP_TRANSFER_DECODING:							
+					case 136://CURLOPT_IGNORE_CONTENT_LENGTH:
+					case 111://CURLOPT_PROXYAUTH:
+					case 206://CURLOPT_TLSAUTH_TYPE:							
+					case 107://CURLOPT_HTTPAUTH:
+					case 51://CURLOPT_NETRC:
+					case 215://CURLOPT_TCP_KEEPINTVL:						
+					case 214://CURLOPT_TCP_KEEPIDLE:						
+					case 213://CURLOPT_TCP_KEEPALIVE:
+					case 171://CURLOPT_ADDRESS_SCOPE:						
+					case 121://CURLOPT_TCP_NODELAY:
+					case 3://CURLOPT_PORT:
+					case 98://CURLOPT_BUFFERSIZE:
+					case 92://CURLOPT_DNS_CACHE_TIMEOUT:
+					case 140://CURLOPT_LOCALPORTRANGE:							
+					case 139://CURLOPT_LOCALPORT:						
+					case 180://CURLOPT_SOCKS5_GSSAPI_NEC:							
+					case 61://CURLOPT_HTTPPROXYTUNNEL:						
+					case 101://CURLOPT_PROXYTYPE:						
+					case 59://CURLOPT_PROXYPORT:						
+					case 182://CURLOPT_REDIR_PROTOCOLS:						
+					case 181://CURLOPT_PROTOCOLS:
 						
 						curl_easy_setopt(curl, option, atoi((const char *)value.c_str()));	
 						break;
 						//set the body size here, for security
-					case CURLOPT_PUT:						
-					case CURLOPT_POST:
-					case CURLOPT_UPLOAD:						
+					case 54://CURLOPT_PUT:						
+					case 47://CURLOPT_POST:
+					case 46://CURLOPT_UPLOAD:						
 						curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, Param4InData.getBytesLength());
 						curl_easy_setopt(curl, option, atoi((const char *)value.c_str()));	
 						break;						
-						//comma to linked list; to be implemented						
-					case CURLOPT_MAIL_RCPT:		
-					case 187:							
-						//		case CURLOPT_QUOTE:		
-						//		case CURLOPT_POSTQUOTE:						
-						//		case CURLOPT_PREQUOTE:	
-						//		case CURLOPT_RESOLVE:						
-						//		case CURLOPT_HTTP200ALIASES:	
 						
+					case 187://CURLOPT_MAIL_RCPT							
+                        
 						for(unsigned int p = 0, n; p <= value.length(); p = n + 1){
 							
 							n = value.find_first_of((const uint8_t *)",", p);
 							if(n == CUTF8String::npos) n = value.length();
 							CUTF8String v = value.substr(p, n - i);
 							values_CURLOPT_MAIL_RCPT = curl_slist_append(values_CURLOPT_MAIL_RCPT, (const char *)v.c_str());
-							
+                            
 						}	
 						
 						if(values_CURLOPT_MAIL_RCPT) curl_easy_setopt(curl, option, values_CURLOPT_MAIL_RCPT);
-						
-						break;		
-						
+                        
+						break;	
+                        
+						//comma to linked list; not implemented                        
+                        //                    case CURLOPT_QUOTE:		
+                        //                    case CURLOPT_POSTQUOTE:						
+                        //                    case CURLOPT_PREQUOTE:	
+                        //                    case CURLOPT_RESOLVE:						
+                        //                    case CURLOPT_HTTP200ALIASES:
+                        //						break;
+                        
 					default:
 						curl_easy_setopt(curl, option, value.c_str());							
 						break;
 				}
 				
 			}
-			
+            
 		}
 		
-		error = curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-		
+        if(values_CURLOPT_HTTPHEADER) curl_easy_setopt(curl, CURLOPT_HTTPHEADER, values_CURLOPT_HTTPHEADER);
+        
+		curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        
 		error = curl_easy_perform(curl);
-		
+        
 		if(values_CURLOPT_MAIL_RCPT) curl_slist_free_all(values_CURLOPT_MAIL_RCPT);
-		if(values_CURLOPT_AUTHORIZATION) curl_slist_free_all(values_CURLOPT_AUTHORIZATION);
+		if(values_CURLOPT_HTTPHEADER) curl_slist_free_all(values_CURLOPT_HTTPHEADER);
+		if(post_CURLOPT_HTTPPOST) curl_formfree(post_CURLOPT_HTTPPOST);
 		
 		curl_easy_cleanup(curl);		
-		
+        
 	}	
 	
 	if(error != CURLE_OK) returnValue.setIntValue(error);	
@@ -333,4 +416,3 @@ void _cURL(sLONG_PTR *pResult, PackagePtr pParams)
 	Param5OutData.toParamAtIndex(pParams, 5);
 	returnValue.setReturn(pResult);
 }
-
